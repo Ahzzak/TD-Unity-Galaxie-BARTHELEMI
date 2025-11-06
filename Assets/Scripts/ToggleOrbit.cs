01553fc61f78998ffe745de2da4904e34d3df0b6
Elrewynn <audreywagner@laposte.net>
2025-11-06 11:07:03 +0100

    Added a skybox & orbit script

--- Assets/Scripts/ToggleOrbit.cs
+++ Assets/Scripts/ToggleOrbit.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class ToggleOrbit : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+        // Touche utilisée pour activer/désactiver
+        public KeyCode toggleKey = KeyCode.Space;
+
+        // État de l’orbite (activé ou non)
+        private bool orbitEnabled = false;
+   
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Quand on appuie sur la touche définie
+        if (Input.GetKeyDown(toggleKey))
+        {
+
+            // On inverse l’état (on “toggle”)
+            orbitEnabled = !orbitEnabled;
+
+            Debug.Log("Orbit mode: " + (orbitEnabled ? "ON" : "OFF"));
+        }
+        // Si le mode orbite est activé, on exécute un comportement
+        if (orbitEnabled)
+        {
+            transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
+        }
     }
 }
